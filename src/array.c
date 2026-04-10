f84706f3d5e8e9b02307c0e1efcbfff3d8e819ee
code idea chill <help.serval@gmail.com>
2026-04-10 23:08:51 +0100

    committed by adem

--- src/array.c
+++ src/array.c
@@ -1,17 +1,6 @@
 #include<stdio.h>
-<<<<<<< HEAD
 
 int main(){
-
-
-
-
-
-
-
-
-=======
-int main(){
->>>>>>> 37ee06ea7f0a6beff85bdb6a0eeeae6804da5657
+    printf("code of array");
     return 0;
 }
--- src/file_utils.c
+++ src/file_utils.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 
 int main(){
- printf("by menouar");
+    printf("by menouar");
 
     return 0;
 }
