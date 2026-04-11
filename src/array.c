9d2fccff51f60073fa02097bd81a50fb80af007c
code idea chill <help.serval@gmail.com>
2026-04-11 21:16:56 +0100

    bubble sort

--- src/array.c
+++ src/array.c
@@ -57,16 +57,13 @@ void swap(int* x, int* y){
 
 //bubble sort sotring each two element
 void bubbleSort(int arr[], int size){
-    bool swapped;
+   
     for(int i = 0; i < size - 1; ++i){
-        swapped = false;
         for(int j = 0; j < size - i - 1; ++j){
             if(arr[j] > arr[j + 1]){
                 swap(&arr[j], &arr[j + 1]);
-                swapped = true;
             }
         }
-        if(swapped == false)break;
     }
 }
 
