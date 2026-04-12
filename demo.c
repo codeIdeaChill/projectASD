cd5ddfd3f81bf06adaad945b9a1de55345aa1a2a
MENOUAR <abdelmounaimmenouar10@gmail.com>
2026-04-12 10:03:45 +0100

    ffff

--- demo.c
+++ demo.c
@@ -1,20 +1,5 @@
-<<<<<<< HEAD
-#include<stdio.h>
-
-int main(){
-
-
-
-
-
-
-
-
-    return 0;
-=======
 #include <stdio.h>
 
 int main(){
     printf("hello from linux");
->>>>>>> 31e6d85c50b16c8da94105b9c41e3262ef5d12ca
 }
--- src/array.c
+++ src/array.c
@@ -1,17 +1,4 @@
 #include<stdio.h>
-<<<<<<< HEAD
-
 int main(){
-
-
-
-
-
-
-
-
-=======
-int main(){
->>>>>>> 37ee06ea7f0a6beff85bdb6a0eeeae6804da5657
     return 0;
 }
