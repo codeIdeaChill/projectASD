471523df08b5c82b179fce8b0bcdf7bb26553efa
code idea chill <help.serval@gmail.com>
2026-04-15 01:00:04 +0100

    merge sort

--- src/array.c
+++ src/array.c
@@ -106,6 +106,60 @@ void insertionSort(int arr[], int size){
 }
 
 
+void merge(int arr[], int l, int m, int r){
+
+    int n1 = m - l + 1;
+    int n2 = r - m;
+    
+    int L[n1], R[n2];
+
+    for (int i = 0; i < n1; ++i){
+        L[i] = arr[l + i];
+    }
+    for (int j = 0; j < n2; ++j){
+        R[j] = arr[m + 1 +j];
+    }
+
+
+    int i =0, j=0, k =l;
+
+    while(i < n1 && j < n2){
+        if(L[i] <= R[j]){
+            arr[k] = L[i];
+            i++;
+        }else{
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    while(i < n1){
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+    while(j < n2){
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+
+}
+
+void mergeSort(int arr[], int left, int right){
+    if(left < right){
+        int m = left + (right - left)/2;
+
+        mergeSort(arr, left, m);
+        mergeSort(arr, m +1, right);
+
+        merge(arr, left, m, right);
+    }
+}
+
+
+
 int binarySearch(int arr[], int size, int value){
     int low = 0, hight = size - 1;
     int mid = (hight + low) / 2;
@@ -159,7 +213,8 @@ int main(){
 
     
     //bubbleSort(arr, size);// this func for sort the array
-    insertionSort(arr,size);
+    //insertionSort(arr,size);
+    mergeSort(arr,0,size - 1);
     printArray(arr, size);// to print the array
 
     printf("the value in %d position \n", binarySearch(arr, size, 1)); 
