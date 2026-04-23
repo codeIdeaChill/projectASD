9d3bbe02f144f39c216e0eca4df49ae7de58476d
code idea chill <help.serval@gmail.com>
2026-04-23 22:15:37 +0100

    linked

--- src/linked_list.c
+++ src/linked_list.c
@@ -11,59 +11,18 @@ void initList(Node* list){
 }
 
 int insertBeginning(Node* list, int value){
-    list->data = value;
     Node* newnode = (Node*)malloc(sizeof(Node*));
+    newnode->data = value;
     newnode->next = list; 
 
 }
 
 
-int insertEnd(ArrayList* list, int value){
-    if(list->size >= MAX_LIST_SIZE){
-        printf("List is full");
-        return Null;
-    }
-    int index = list->size;
-    list->data[index] = value;
-    list->next[index] = Null;
-    if(list->head == Null){
-        list->head = index;
-    }else{
-        int current = list->head;
-        while(list->next[current] != Null){
-            current = list->next[current]; 
-        }
-        list->next[current] = index;
-    }
-    list->size++;
-}
 
-int insertAtPosition(ArrayList* list, int pos, int value){
-    int index = findFree(*list);
-    if(index == Null){
-        printf("List is Full \n");
-        return Null;
-    }
-    if(pos == 0){
-        list->data[index] = value; 
-        list->next[index] = list->head;
-        list->head = index;
-    }else{
-        list->data[index] = value; 
-        list->next[index] = list->next[pos - 1];
-        list->next[pos - 1] = index;
-    }
-    list->size++;
-
-}
-
-void displayList(ArrayList* list){
-    int index = list->head;
-    int i = 0;
-    while(index != Null && i < list->size){
-        printf("%d -> ", list->data[index]);
-        index = list->next[index];
-        i++;
+void displayList(Node* list){
+    while(list->next != NULL){
+        printf("%d -> ", list->data);
+        list = list->next;
     } 
     printf("NULL! ");
 }
@@ -71,12 +30,12 @@ void displayList(ArrayList* list){
 
 
 int main(){
-    ArrayList list;
+    Node list;
     initList(&list);
-    insertEnd(&list, 4);
-    insertEnd(&list, 6);        
-    insertEnd(&list, 2);
-    insertEnd(&list, 3);
+    insertBeginning(&list, 4);
+    insertBeginning(&list, 6);        
+    insertBeginning(&list, 2);
+    insertBeginning(&list, 3);
 
     
     /*
@@ -86,7 +45,5 @@ int main(){
     displayList(&list); 
 
 
-    insertAtPosition(&list, 3, 5);
-    displayList(&list); 
     return 0;
 }
